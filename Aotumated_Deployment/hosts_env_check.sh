de2be457d5ef76700a5c098286ab75a30209fc92
luqimin2005 <luqimin2005@163.com>
2019-01-07 22:23:45 +0800

    Hosts Env Checking

--- Aotumated_Deployment/hosts_env_check.sh
+++ Aotumated_Deployment/hosts_env_check.sh
@@ -2,4 +2,12 @@
 
 
 export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook hosts_env_check.yml
+ansible-playbook hosts_env_check.yml --tags=$1
+
+# Args Input:
+#   all                :  Run all the requried services checking.
+#   firewalld_checking :  Run firewalld service status checking only.
+#   selinux_checking   :  Run selinux status checking only.
+#   timedate_checking  :  Run ntp service & ntp state checking only.
+#   jdk_checking       :  Run jdk version checking only.
+#   thp_checking       :  Run THP checking only.
